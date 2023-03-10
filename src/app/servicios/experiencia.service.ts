904749429f774a5d0971edc2c24e76eecc7d56c3
aguadis <guadi1404@gmail.com>
2023-03-10 02:04:05 -0300

    cambios

--- src/app/servicios/experiencia.service.ts
+++ src/app/servicios/experiencia.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 // es para suscribirse y que se reciba respuesta de forma asincronica
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 //es para poder hacer peticiones
 import { HttpClient } from '@angular/common/http';
 import { Experiencia } from '../entidades/experiencia';
