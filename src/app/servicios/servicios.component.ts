587396a18f984865af8321445ddc62748adeb86e
aguadis <guadi1404@gmail.com>
2023-03-10 23:43:29 -0300

    cambios

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServiciosComponent {
   sServicios: any;
   servicios: any;
-  url = 'https://portfolio-cintia-necol.onrender.com';
+  url: string= "https://portfolio-cintia-necol.onrender.com";
  
   }
   
